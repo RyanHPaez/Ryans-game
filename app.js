f01e35cb6c7218a63d9bad378f2f8028690941ec
ryan paez <solosnail@gmail.com>
2022-02-25 21:45:24 -0800

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // create Track
 // create background
 // create Rats
+
 // create start line
 // create motion
+
 // create finish line
 // create user
 // create bets on Rats
@@ -10,3 +12,15 @@
 // create winner
 // create loser
 // create tie
+
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
+
+function test(){
+    sleep(5000)
+        .then(() => console.log('Hello World'))
+}
