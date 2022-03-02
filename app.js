027509cd9ebe4b1a977f78924145637258547f91
ryan paez <solosnail@gmail.com>
2022-03-01 22:00:25 -0800

    5th commit

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-let ratlist = []
-var finishOrder = []
-
 window.onload = (event) => {
     ratList= Array.from(document.getElementById("rats").children).map((ratElement)=> new Rat(ratElement.id));
     console.log ('-------------ratList', ratList)
@@ -16,6 +13,7 @@ function startRace() {
                
             
     })
+
 }
 
 
--- rat.js
+++ rat.js
@@ -8,18 +8,8 @@ class Rat {
         this.finishedStatus = null
 }
 
-//restarts race
-reset() {
-    
-    clearInterval(this.finishedStatus);
-    this.ratElement= document.getElementById(this.id);
-    this.ratElement.style.left = "0px";
-    
-    
 
-    
-    
-}
+
                       
 
 
@@ -39,28 +29,11 @@ run() {
             this.ratElement= document.getElementById(this.id);
             this.finishedStatus = false
         }
+        
     }, 
     1000/(Math.random() * 20 + 15));
     }
 }
-
-//   var betRat, amount, funds;  //Global variables
-
-// document.getElementById('startRace').onclick = function () {
-//     //Get the values from the DOM
-//     betRat = document.getElementById('betRat').value;
-//     amount = parseInt(document.getElementById('amount').value);
-//     funds = parseInt(document.getElementById('funds').innerText);
-
-//     //Basic input validation
-//     if (amount > funds) {
-//         alert("You do not have enough funds.");
-//         return 0;
-    
-//     } else if (amount < 1) {
-//         alert("Bet amount must be greater than 0.");
-//         return 0;
-//     }
 
 
 
