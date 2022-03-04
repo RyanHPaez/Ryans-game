122d70061e27f67f74268ac8efd8f43ff9b69c9a
ryan paez <solosnail@gmail.com>
2022-03-04 13:50:57 -0800

    9th commit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-window.onload = (event) => {
-    ratList= Array.from(document.getElementById("rats").children).map((ratElement)=> new Rat(ratElement.id));
-    console.log ('-------------ratList', ratList)
-    
-}
+
 
 function startRace() {
+    
+
+
+    ratList= Array.from(document.getElementById("rats").children).map((ratElement)=> new Rat(ratElement.id));
+    console.log ('ratListArray', ratList);
+
     ratList.forEach((rat) => {
-                rat.run()
-               
-            
+                rat.run();            
     })
 
 }
@@ -16,8 +16,44 @@ function startRace() {
 
 
 
+// function RaceWinners() {
+//     console.log("0")
+//     return new Promise(resolve => {
+//       setInterval(function() {
+//           resolve(1)
+//         }, 0)
+//     })
+//   }
+//   console.log ("1")
+
+//   async function Start() {
+//     console.log('== START==')
+//     Start() // starts right away"
+//   }
+
+//     async function resolveAfter2Seconds() {
+//         // console.log("ratlist") ok          
+//       }
+//       const winners =  RaceWinners()
+//       this.ratElement = document.getElementById(this.id)
+//       console.log ("2")
+
+
+
+
   
 
+
+    
+//     alert('1');
+//   }, 500);
+  
+  // With a function defined separately
+//   function sayHi() {
+//     alert('2');
+//   }
+  
+ 
     //------------------------------------------------------------------------------
 
   
--- rat.js
+++ rat.js
@@ -1,83 +1,47 @@
 
-//main rat class------------------
 class Rat { 
     constructor(id) {
         this.id = id
         this.movementLoop = true
         this.ratElement = document.getElementById(this.id)
         this.finishedStatus = null
+        this.finishOrder = []
 }                    
 
 run() {
 
-
     
-    this.movementLoop = setInterval(() => {
+          
+      
 
-            //  setTimeout(startRace()){
-//  if ((left+ width) < 1700) 
-// timer = setTimeout ("starRace"(),1700);
-// }
-// else if ((left+width)< 1700 ){
-    // clearTimeout(timer);
-//    
-// }
-        var step = 35; // this is the speed
+     this.movementLoop = setInterval(() => {
 
-        var xPosition = this.ratElement.offsetLeft;
+ 
+   
+    var step =7; // this is the speed
 
-        if (xPosition < 1700)
+    var xPosition = this.ratElement.offsetLeft;
+
+    if (xPosition < 1700)
+    {// how far
+        xPosition = xPosition + step;
+        this.ratElement.style.left = xPosition + "px"; //horizontal movement
+    } else {
         
-        {// how far
-            xPosition = xPosition + step;
-            this.ratElement.style.left = xPosition + "px"; //horizontal movement
-            
-        } else { 
-            this.ratElement= document.getElementById(this.id);
-            this.finishedStatus = true;
-            
-        } 
+        this.finishedStatus = true;
+        this.finishOrder.push(this.id) 
+        alert (this.id  );
+        // reload();
         
-    }, 
-    1000/(Math.random() * 20 + 15)); 
+
+       
+
+        // run logic here to .push(this.id) into the finish array
     }
-
+    
+    
+},
+100/(Math.random() * 20 + 15));
+// console.log("ratEmenlet")
+}   
 }
-
-// 
-// function resolveAfter2Seconds() {
-//   console.log("ratlist")
-
-
-
-
-
-//   })
-// }
-// 
-// function resolveAfter1Second() {
-//   console.log("starting fast promise")
-//   return new Promise(resolve => {
-    // setTimeout(function() {
-    //   resolve("rat3")
-    //   console.log("rat4")
-    // }, 1700)
-//   })
-// }
-// 
-// async function sequentialStart() {
-//   console.log('==SEQUENTIAL START==')
-// 
-//   1. Execution gets here almost instantly
-//   const slow = await resolveAfter2Seconds()
-//   console.log(slow) // 2. this runs 2 seconds after 1.
-// 
-//   const fast = await resolveAfter1Second()
-//   console.log(fast) // 3. this runs 3 seconds after 1.
-// }
-// 
-// 
-// 
-// sequentialStart() // after 2 seconds, logs "slow", then after 1 more second, "fast"
-// 
-// 
